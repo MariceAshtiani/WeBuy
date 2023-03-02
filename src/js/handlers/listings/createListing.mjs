23446a127bed86eebd806a252503fd5954ab7e05
MariceAshtiani <95356770+MariceAshtiani@users.noreply.github.com>
2023-03-02 15:57:31 +0100

    added mediaValues to get the array of images

--- src/js/handlers/listings/createListing.mjs
+++ src/js/handlers/listings/createListing.mjs
@@ -6,15 +6,18 @@ export function setCreateListingFormListener() {
 
     if (form) {
         form.addEventListener("submit", (event) => {
-            event.preventDefault()
+            event.preventDefault();
 
             const form = event.target;
             const formData = new formData(form);
+            const mediaValues = formData.getAll('media[]');
             const listing = object.fromEntries(formData.entried())
             post.tags = convertTags(listing.tags)
             const action = form.action;
             const method = form.method;
 
+
+            console.log(mediaValues);
             createListing(listing)
         })
     }
