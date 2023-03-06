ed7d69c337faf8c402287240550cd41ec3bf917e
MariceAshtiani <95356770+MariceAshtiani@users.noreply.github.com>
2023-03-06 12:08:49 +0100

    fixed errors

--- src/js/handlers/listings/createListing.mjs
+++ src/js/handlers/listings/createListing.mjs
@@ -2,20 +2,19 @@ import { createListing } from "../../api/listings/create.mjs";
 import { convertTags } from "../tags.mjs";
 
 export function setCreateListingFormListener() {
-    const form = document.querySelector("#CreateListing");
+    const form = document.querySelector("#createListing");
 
     if (form) {
         form.addEventListener("submit", (event) => {
             event.preventDefault();
 
             const form = event.target;
-            const formData = new formData(form);
+            const formData = new FormData(form);
             const mediaValues = formData.getAll('media[]');
-            const listing = object.fromEntries(formData.entried())
-            post.tags = convertTags(listing.tags)
+            const listing = Object.fromEntries(formData.entries())
+            listing.tags = convertTags(listing.tags)
 
 
-            console.log(mediaValues);
             createListing(listing)
         })
     }
