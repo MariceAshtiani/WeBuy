32fbc889ce8e848acd570883bcb011d69dccb58f
MariceAshtiani <95356770+MariceAshtiani@users.noreply.github.com>
2023-03-09 18:58:16 +0100

    created a auth/notauth nav

--- src/js/api/auth/nav.mjs
+++ src/js/api/auth/nav.mjs
@@ -1,6 +1,53 @@
-//export function checkAuth() {
-//    const isLoggedIn = (token)
-// if (token !== null)
-// show everything int he nav
-// else show less
-//}
+export function checkAuth() {
+    const token = localStorage.getItem("token");
+    const navMenu = document.querySelector(".nav-menu");
+
+
+    if (token != null) {
+        navMenu.innerHTML = `<li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="/index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/listings/">Listings</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/listing/create/">New listing</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle"  data-bs-toggle="dropdown"
+                                aria-expanded="false">Profile</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="/profile/">View Profile</a></li>
+                                <li><a class="dropdown-item" href="/profile/edit/">Edit Profile</a></li>
+                                <li><a class="dropdown-item logout" >Log out</a></li>
+                            </ul>
+                        </li>
+                            `;
+    } else {
+        navMenu.innerHTML = `<li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="/index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/profile/register/">Register</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/profile/login/">Log In</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/listings/">Listings</a>
+                            </li>
+                            `;
+    }
+
+    const navLinks = document.querySelectorAll(".nav-link")
+    const pathname = window.location.pathname;
+
+    navLinks.forEach(navLink => {
+        if (navLink.href.includes(pathname)) {
+            navLink.classList.add('active');
+        } else {
+            navLink.classList.remove('active');
+        }
+    })
+}
+
