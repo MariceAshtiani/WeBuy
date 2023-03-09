edac046dbb767cae3ed289bef1ad5295bcaf58ca
MariceAshtiani <95356770+MariceAshtiani@users.noreply.github.com>
2023-03-09 18:57:32 +0100

    added new functions to the router

--- src/js/router.mjs
+++ src/js/router.mjs
@@ -1,6 +1,8 @@
 import * as listeners from "./handlers/index.mjs";
 import * as templates from "./templates/index.mjs";
 import * as listings from "./api/listings/index.mjs";
+import { checkAuth } from "./api/index.mjs";
+import { notLoggedIn } from "./api/ui/welcome.mjs";
 
 export default function router() {
 
@@ -9,38 +11,68 @@ export default function router() {
     switch (path) {
         case '/':
         case '/index.html':
+            checkAuth();
+            notLoggedIn();
             templates.displayFeaturedListings();
+            listeners.setLogoutListener();
+
             break;
         case '/profile/register/':
         case '&profile/register/index.html':
+            checkAuth();
             listeners.setRegisterFormListener();
+            listeners.setLogoutListener();
+
             break;
         case '/profile/login/':
         case '/profile/login/index.html':
+            checkAuth();
             listeners.setLoginFormListener();
+            listeners.setLogoutListener();
+
             break;
         case '/listings/':
         case '/listings/index.html':
+            checkAuth();
             templates.displayListings();
             listeners.setLogoutListener();
+
             break;
         case '/listing/':
         case '/listing/index.html':
+            checkAuth();
             templates.viewSingleListing();
             listeners.createBidListener();
             listeners.setLogoutListener();
+
             break;
         case '/listing/create/':
         case '/listing/create/index.html':
+            checkAuth();
             listeners.setCreateListingFormListener();
             listeners.setAddMoreMediaListener();
             listeners.setLogoutListener();
+
             break;
         case '/listing/edit/':
         case '/listing/edit/index.html':
+            checkAuth();
             listeners.setUpdateListingFormListener();
             listeners.setAddMoreMediaListener();
             listeners.setRemoveListingListener();
             listeners.setLogoutListener();
+            break;
+        case '/profile/':
+        case '/profile/index.html':
+            checkAuth();
+            listeners.setLogoutListener();
+            break;
+        case '/profile/edit':
+        case '/profile/edit/index.html':
+            checkAuth();
+            listeners.setLogoutListener();
+            break;
+
+
     }
 }
